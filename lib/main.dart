9c2c54776ff17a42fe64781efae82680bca30b23
User_Y3V <a.22308051280565@cbtis128.edu.mx>
2025-04-30 14:23:24 +0000

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:act6_pantallas_6i/pantalla1.dart';
+import 'package:act6_pantallas_6i/pantalla2.dart';
+import 'package:act6_pantallas_6i/pantalla3.dart';
+import 'package:act6_pantallas_6i/pantalla4.dart';
+import 'package:act6_pantallas_6i/pantalla5.dart';
+import 'package:act6_pantallas_6i/pantalla6.dart';
+import 'package:act6_pantallas_6i/pantalla7.dart';
+import 'package:act6_pantallas_6i/pantalla8.dart';
+import 'package:act6_pantallas_6i/pantalla9.dart';
+import 'package:act6_pantallas_6i/pantalla10.dart';
+import 'package:act6_pantallas_6i/pantalla11.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisPantallas());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisPantallas extends StatelessWidget {
+  const MisPantallas({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'rutas entre paginas',
+      initialRoute: '/',
+      routes: {
+        // When haces tus momos en los comentarios
+        '/': (context) => const PantallaUno(),
+        // El furroto es hoy oiste viejo
+        '/pantalla2': (context) => const PantallaDos(),
+        '/pantalla3': (context) => const PantallaTres(),
+        '/pantalla4': (context) => const PantallaCuatro(),
+        '/pantalla5': (context) => const PantallaCinco(),
+        '/pantalla6': (context) => const PantallaSeis(),
+        '/pantalla7': (context) => const PantallaSiete(),
+        '/pantalla8': (context) => const PantallaOcho(),
+        '/pantalla9': (context) => const PantallaNueve(),
+        '/pantalla10': (context) => const PantallaDiez(),
+        '/pantalla11': (context) => const PantallaOnce(),
+      },
     );
   }
 }
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+// Aquí estuvo santiago sanchez tinoco de la tercera constitucion papasquiaro gonzalez muñez
+// - kevin
---
+++ lib/pantalla1.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class PantallaUno extends StatelessWidget {
+  const PantallaUno({Key? key}) : super(key: key);
+//
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xff987263),
+          title: const Text(
+            'Pantalla 1',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            SizedBox(height: 15),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pantalla2');
+                },
+                child: const Text('Pantalla Dos'),
+              ),
+            ),
+            SizedBox(height: 5),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pantalla3');
+                },
+                child: const Text('Pantalla Tres'),
+              ),
+            ),
+            SizedBox(height: 5),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pantalla4');
+                },
+                child: const Text('Pantalla Cuatro'),
+              ),
+            ),
+            SizedBox(height: 5),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pantalla5');
+                },
+                child: const Text('Pantalla Cinco'),
+              ),
+            ),
+            SizedBox(height: 5),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pantalla6');
+                },
+                child: const Text('Pantalla Seis'),
+              ),
+            ),
+            SizedBox(height: 5),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pantalla7');
+                },
+                child: const Text('Pantalla Siete'),
+              ),
+            ),
+            SizedBox(height: 5),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pantalla8');
+                },
+                child: const Text('Pantalla Ocho'),
+              ),
+            ),
+            SizedBox(height: 5),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pantalla9');
+                },
+                child: const Text('Pantalla Nueve'),
+              ),
+            ),
+            SizedBox(height: 5),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pantalla10');
+                },
+                child: const Text('Pantalla Diez'),
+              ),
+            ),
+            SizedBox(height: 5),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/pantalla11');
+                },
+                child: const Text('Pantalla Once'),
+              ),
+            ),
+          ],
+        ));
+  }
+}
